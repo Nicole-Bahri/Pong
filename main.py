5c7e516d52b27217fc1ca1d1eeb8504fcc6f7f06
Nicole Bahri <bahrinicole1@gmail.com>
2022-10-03 16:02:04 -0400

    Title and icon :)

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ screen_height = 600
 size =(screen_width, screen_height)
 #Display window
 screen = pygame.display.set_mode(size)
+#TITLE
+pygame.display.set_caption('Pong game')
+#icon
+icon = pygame.image.load('PIN PONG.png')
+pygame.display.set_icon(icon)
 #game loop
 running = True
 while running: 
