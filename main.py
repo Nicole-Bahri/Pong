610d6ae6d87aae5dec7e16547f62fedae4a46844
Nicole Bahri <bahrinicole1@gmail.com>
2022-10-17 15:16:50 -0400

    Players boundaries and ball movement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-#Import pygame
+#Import
+from turtle import speed
 import pygame
+import random as rd
 pygame.init()
 #Colors
 white = (255, 255, 255) 
@@ -24,6 +26,8 @@ player_2_y_speed = 0
 ball_x = 400
 ball_y = 300
 ball_radius = 20
+ball_speed_x = 1.5
+ball_speed_y = 1.5
 #size variable
 size =(screen_width, screen_height)
 #Display window
@@ -67,16 +71,47 @@ while running:
     #players movement
     player_1_y += player_1_y_speed
     player_2_y += player_2_y_speed
+    #player boundaries
+    #player 1
+    if player_1_y <= 0:
+        player_1_y = 0
+    if player_1_y >= screen_height - players_height:
+        player_1_y = screen_height - players_height
+    #player 2
+    if player_2_y <= 0:
+        player_2_y = 0
+    if player_2_y >= screen_height - players_height:
+        player_2_y = screen_height - players_height
+    #ball movement
+    ball_x += ball_speed_x
+    ball_y += ball_speed_y
+    #ball boundaries
+    if ball_y >(screen_height - ball_radius) or ball_y < ball_radius:
+        ball_speed_y *= -1
+    #rigth or left and score update
+    if ball_x > screen_width :
+        ball_x = screen_width/2
+        ball_y = screen_width/2
+        ball_speed_x *= rd.choice([-1, 1]) 
+    elif ball_x < 0 :
+        ball_x = screen_width/2
+        ball_y = screen_width/2
+        ball_speed_x *= rd.choice([-1, 1]) 
+
+      
     #fill the screen with color
     screen.fill(white)
     #Drawing area 
     #Define player 1 left : a rectangle
     player_1 = pygame.draw.rect(screen, color_player, (player_1_x, player_1_y, players_width, players_height))
     player_2 = pygame.draw.rect(screen, color_player, (player_2_x, player_2_y, players_width, players_height))
-    #Draw the ball
-    pygame.draw.circle(screen, color_ball,(ball_x,ball_y),ball_radius)
     #Draw the center line
     pygame.draw.aaline(screen, color_line, (screen_width/2, 0), (screen_width/2, screen_height))
+    #Draw the ball
+    ball = pygame.draw.circle(screen, color_ball,(ball_x,ball_y),ball_radius)
+    #Colitions 
+    if ball.colliderect(player_1) or ball.colliderect(player_2):
+        ball_speed_x *= -1
     # Refresh the windows
     pygame.display.flip()
     
